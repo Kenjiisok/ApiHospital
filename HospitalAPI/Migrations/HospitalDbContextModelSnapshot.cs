275bdee6efaf71eaa346be3bfd61231d9c8cc740
Kenjiisok <dudu.hirose@hotmail.com>
2023-09-27 11:59:30 -0300

    bg card

--- HospitalAPI/Migrations/HospitalDbContextModelSnapshot.cs
+++ HospitalAPI/Migrations/HospitalDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace HospitalAPI.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.10")
+                .HasAnnotation("ProductVersion", "7.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             OracleModelBuilderExtensions.UseIdentityColumns(modelBuilder);
