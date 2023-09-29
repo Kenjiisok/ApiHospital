b94f7e8435de8694bd257965070f862a19123d49
Eduardo Kenji Pageu Hirose <dudu.hirose@hotmail.com>
2023-09-29 15:50:53 -0300

    mudancas na index e no feedback

--- HospitalAPI/wwwroot/js/site.js
+++ HospitalAPI/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// feedback.js
+
+var stars = document.querySelectorAll('.star');
+stars.forEach(function (star) {
+    star.addEventListener('mouseover', function () {
+        var rating = star.getAttribute('data-rating');
+        highlightStars(rating);
+    });
+
+    star.addEventListener('mouseout', function () {
+        var currentRating = document.getElementById('avaliacao').value;
+        highlightStars(currentRating);
+    });
+
+    star.addEventListener('click', function () {
+        var rating = star.getAttribute('data-rating');
+        document.getElementById('avaliacao').value = rating;
+    });
+});
+
+function highlightStars(rating) {
+    stars.forEach(function (star) {
+        var starRating = star.getAttribute('data-rating');
+        if (starRating <= rating) {
+            star.classList.add('highlighted'); // Adicione a classe 'highlighted'
+        } else {
+            star.classList.remove('highlighted'); // Remova a classe 'highlighted'
+        }
+    });
+}
